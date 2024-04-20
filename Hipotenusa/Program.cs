7847cab519250c6a1ffd6bcbb60fa672e75b12bf
JulianaMistron <jumistron@yahoo.com.br>
2024-04-20 14:07:58 -0300

    Cria hipotenusa

--- Hipotenusa/Program.cs
+++ Hipotenusa/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double cateto1, cateto2;
+
+Console.WriteLine("Digite o primeiro cateto:");
+cateto1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o segundo cateto:");
+cateto2 = Convert.ToDouble(Console.ReadLine());
+
+double hipotenusa = Math.Sqrt((cateto1 * cateto1) + (cateto2 * cateto2));
+
+Console.WriteLine("A hipotenusa é: " + hipotenusa);
+
+Console.ReadLine();
